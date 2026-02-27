ecb07157e5a8a348a191f0cef3ffea6333371f16
abelUbuntuPC <abel07110711@gmail.com>
2026-02-26 23:15:17 -0500

    modify logging setup

--- etl_project/config/logging.py
+++ etl_project/config/logging.py
@@ -1,7 +1,22 @@
 import logging
+from config.settings import LOG_FORMAT
+import os
 
 def setup_logging():
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(current_dir) # up one level to get to project root
+    
+    log_dir = os.path.join(project_root, "logs") # logs folder
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
+        format=LOG_FORMAT,
+        handlers=[
+            logging.FileHandler(f"{log_dir}/etl_process.log"), # save in file
+            logging.StreamHandler() # show in console
+        ]
     )
